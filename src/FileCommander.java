0062ba5657a4ac8804a2d35ab5b3d8cc60f24778
Olek <olekston@gmail.com>
2023-05-08 19:59:57 +0200

    zad 2

--- src/FileCommander.java
+++ src/FileCommander.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileCommander {
     private Path path;
@@ -11,5 +18,22 @@ public class FileCommander {
     }
     public void cd(String path){
         this.path = this.path.resolve(path);
+    }
+    public List<String> ls(){
+        try {
+            Stream<Path> stream = Files.list(this.path);
+            return stream.sorted((p1,p2) -> p1.compareTo(p2))
+                    .sorted((p1,p2) -> Boolean.compare(Files.isDirectory(p2),Files.isDirectory(p1)))
+                    .map((p) -> {
+                        if(Files.isDirectory(p)){
+                            return "["+p.getFileName().toString()+"]";
+                        }
+                        else{
+                            return p.getFileName().toString();
+                        }
+                    }).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
